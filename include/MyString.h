3dfed6db9f2b0ac9b5dad48ff83403347dde6b59
danek0100 <71225718+danek0100@users.noreply.github.com>
2020-12-07 15:10:10 +0300

    Constructor + Destructor + lenght()

--- include/MyString.h
+++ include/MyString.h
@@ -6,8 +6,36 @@
 #include <string>
 
 class MyString {
- private:
- public:
+private:
+    char* string;
+    unsigned int len;
+public:
+    MyString();
+    MyString(const char*);
+    MyString(std::string);
+    MyString(const MyString&);
+    MyString(MyString&&);
+    ~MyString();
+    unsigned int length();
+    //    get() - возвращение указателя на данные(тип char*).
+    //    Реализовать в классе MyString перегрузку операций :
+
+    //operator+ -сложение(конкатенация двух строк).
+    //    operator- -вычитание(из первой строки удаляются все символы, присутствующие во второй строке).
+    //    operator* -умножение на целое число(строка повторяется заданное число раз).
+    //    operator= -копирующее присваивание.
+    //    operator= -перемещающее присваивание.
+    //    operator== -сравнение на равенство.
+    //    operator!= -сравнение на неравенство.
+    //    operator> -лексографическое сравнение .
+    //    operator< -лексографическое сравнение.
+    //    operator>= -лексографическое сравнение.
+    //    operator<= -лексографическое сравнение.
+    //    operator! - у латинских букв меняется регистр.
+    //    operator[] - доступ к символу по индексу.
+    //    operator() - поиск подстроки.
+    //    operator>> -чтение из потока.
+    //    operator>> -запись в поток.
 };
 #endif  // INCLUDE_MYSTRING_H_
 
--- src/MyString.cpp
+++ src/MyString.cpp
@@ -1,4 +1,67 @@
 // Copyright 2020 <Jiiijyyy>
 #include "MyString.h"
 
+MyString::MyString() {
+    this->string = nullptr;
+    this->len = 0;
+}
 
+MyString::MyString(const char* str) {
+    this->string = nullptr;
+    this->len = 0;
+    if (str) {
+        int len = strlen(str);
+        this->len = len;
+        this->string = new char[len + 1];
+        int i = 0;
+        while (i < len) {
+            this->string[i] = str[i];
+            i++;
+        }
+        this->string[i] = '\0';
+    }
+}
+MyString::MyString(std::string str) {
+    int len = str.length();
+    this->len = len;
+    this->string = new char[len + 1];
+    int i = 0;
+    while (i < len) {
+        this->string[i] = str[i];
+        i++;
+    }
+    this->string[i] = '\0';
+}
+
+MyString::MyString(const MyString& str) {
+    this->len = str.len;
+    this->string = new char[this->len + 1];
+    int i = 0;
+    while (i < this->len) {
+        this->string[i] = str.string[i]; //After input operator[] rename
+        i++;
+    }
+    this->string[i] = '\0';
+}
+
+MyString::MyString(MyString&& str) {
+    this->len = str.len;
+    this->string = new char[this->len + 1];
+    int i = 0;
+    while (i < this->len) {
+        this->string[i] = str.string[i]; //After input operator[] rename
+        i++;
+    }
+    this->string[i] = '\0';
+    str.~MyString();
+}
+
+MyString::~MyString() {
+    delete[] this->string;
+    this->string = nullptr;
+    this->len = 0;
+}
+
+unsigned int MyString::length() {
+    return this->len;
+}
