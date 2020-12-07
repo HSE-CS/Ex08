0e3a9813bd2bc85904f5edb2641fcf494cc25934
TiNa2101 <71177685+TiNa2101@users.noreply.github.com>
2020-12-08 02:36:50 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,6 @@ int main() {
   MyString s3;
   s3 = s1 + s2;
   std::cout << s3;
-  std::cout << s3("ll");   // 2
-  std::cout << s3("mama"); // -1
+  std::cout << s3("ll");
+  std::cout << s3("mama");
 }
