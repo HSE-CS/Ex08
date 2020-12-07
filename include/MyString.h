70a9c5ca539114151a2f00af1bc641c8ae543d30
TiNa2101 <71177685+TiNa2101@users.noreply.github.com>
2020-12-08 02:31:05 +0300

    Update MyString.h

--- include/MyString.h
+++ include/MyString.h
@@ -6,32 +6,32 @@
 
 class MyString {
  private:
-   char* symb;
-   int len;
- public:
-   explicit MyString(const char* = nullptr);
-   explicit MyString(std::string);
-   explicit MyString(const MyString&);
-   explicit MyString(MyString&&);
-   ~MyString();
-   int length() const;
-   char* get() const;
-   MyString operator+(const MyString& str);
-   MyString operator-(const MyString& str);
-   MyString operator*(const int num);
-   MyString& operator=(const MyString& str);
-   MyString& operator=(MyString&& str);
-   bool operator==(const MyString& str);
-   bool operator!=(const MyString& str);
-   bool operator>(const MyString& str);
-   bool operator<(const MyString& str);
-   bool operator>=(const MyString& str);
-   bool operator<=(const MyString& str);
-   MyString operator!();
-   char& operator[](int num);
-   int operator()(const char* str);
-   friend std::ostream& operator<<(std::ostream& os, MyString& str);
-   friend std::istream& operator>>(std::istream& is, MyString& str);
+  char* symb;
+  int len;
+  public:
+  explicit MyString(const char* = nullptr);
+  explicit MyString(std::string);
+  explicit MyString(const MyString&);
+  explicit MyString(MyString&&);
+  ~MyString();
+  int length() const;
+  char* get() const;
+  MyString operator+(const MyString& str);
+  MyString operator-(const MyString& str);
+  MyString operator*(const int num);
+  MyString& operator=(const MyString& str);
+  MyString& operator=(MyString&& str);
+  bool operator==(const MyString& str);
+  bool operator!=(const MyString& str);
+  bool operator>(const MyString& str);
+  bool operator<(const MyString& str);
+  bool operator>=(const MyString& str);
+  bool operator<=(const MyString& str);
+  MyString operator!();
+  char& operator[](int num);
+  int operator()(const char* str);
+  friend std::ostream& operator<<(std::ostream& os, MyString& str);
+  friend std::istream& operator>>(std::istream& is, MyString& str);
 };
 
 #endif  // INCLUDE_MYSTRING_H_
