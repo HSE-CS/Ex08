745f3a6959ae966c8fb86502624a40c7eda96379
danek0100 <71225718+danek0100@users.noreply.github.com>
2020-12-07 14:14:08 +0300

    Start

---
+++ include/MyString.h
@@ -0,0 +1,13 @@
+// Copyright 2020 <Jiiijyyy>
+#ifndef INCLUDE_MYSTRING_H_
+#define INCLUDE_MYSTRING_H_
+#include <cmath>
+#include <iostream>
+#include <string>
+
+class MyString {
+ private:
+ public:
+};
+#endif  // INCLUDE_MYSTRING_H_
+
---
+++ src/MyString.cpp
@@ -0,0 +1,4 @@
+// Copyright 2020 <Jiiijyyy>
+#include "MyString.h"
+
+
