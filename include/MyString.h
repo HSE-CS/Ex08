e716b1285b7eb5ff34b522906177fb2180d360dd
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2020-12-07 18:55:19 +0300

    typo

--- include/MyString.h
+++ include/MyString.h
@@ -10,38 +10,55 @@
 class MyString {
  private:
 
-  char *inner;
-  size_t len;
+  char *characters;
+  size_t size;
 
  public:
 
-  MyString(const char *characters);
-  MyString(const std::string& string);
+  MyString(const char *characters = nullptr);
+
+  MyString(const std::string &string);
+
   MyString(const MyString &string);
-  MyString(MyString &&string);
+
+  MyString(MyString &&string) noexcept;
 
   ~MyString();
 
   size_t length() const;
+
   char *get();
 
   MyString operator+(MyString &string);
+
   MyString operator-(MyString &string);
+
   MyString operator*(size_t factor);
+
   MyString operator=(MyString &string);
+
   MyString operator=(MyString &&string);
 
-  bool operator==(const MyString &string);
-  bool operator!=(const MyString &string);
+  bool operator==(const MyString &string) const;
+
+  bool operator!=(const MyString &string) const;
+
   bool operator>(const MyString &string);
+
   bool operator<(const MyString &string);
+
   bool operator>=(const MyString &string);
+
   bool operator<=(const MyString &string);
 
   MyString operator!();
+
   char operator[](size_t index);
+
   int32_t operator()(const char *string);
+
   friend std::istream &operator>>(std::istream &in, MyString &string);
+
   friend std::ostream &operator<<(std::ostream &out, MyString &string);
 
 };
