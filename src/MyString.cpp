ddfacd89fea68e4c6293803226981bec2847f9eb
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2020-12-07 18:54:44 +0300

    remove undone methods

--- src/MyString.cpp
+++ src/MyString.cpp
@@ -1,99 +1,46 @@
 // Copyright by Stanislav on 07/12/2020.
 
+#include <tuple>
 #include "../include/MyString.h"
 
 MyString::MyString(const char *characters) {
-  this->len = strlen(characters);
-  this->inner = strdup(characters);
+  this->size = strlen(characters);
+  this->characters = strdup(characters);
 }
 
 MyString::MyString(const std::string &string) {
-  this->len = string.length();
-  this->inner = strdup(string.c_str());
+  this->size = string.length();
+  this->characters = strdup(string.c_str());
 }
 
 MyString::MyString(const MyString &string) {
-  this->len = string.len;
-  this->inner = strdup(string.inner);
+  this->size = string.size;
+  this->characters = strdup(string.characters);
 }
 
-MyString::MyString(MyString &&string) {
-  this->len = string.len;
-  this->inner = strdup(string.inner);
+MyString::MyString(MyString &&string) noexcept {
+  this->size = string.size;
+  this->characters = strdup(string.characters);
+  this->~MyString();
 }
 
 MyString::~MyString() {
-  free(this->inner);
+  free(this->characters);
+  this->size = 0;
 }
 
 size_t MyString::length() const {
-  return this->len;
+  return this->size;
 }
 
 char *MyString::get() {
-  return this->inner;
+  return this->characters;
 }
 
-MyString MyString::operator+(MyString &string) {
-  return MyString(nullptr);
+bool MyString::operator==(const MyString &string) const {
+  return std::tie(characters, size) == std::tie(string.characters, string.size);
 }
 
-MyString MyString::operator-(MyString &string) {
-  return MyString(nullptr);
-}
-
-MyString MyString::operator*(size_t factor) {
-  return MyString(nullptr);
-}
-
-MyString MyString::operator=(MyString &string) {
-  return MyString(nullptr);
-}
-
-MyString MyString::operator=(MyString &&string) {
-  return MyString(nullptr);
-}
-
-bool MyString::operator==(const MyString &string) {
-  return false;
-}
-
-bool MyString::operator!=(const MyString &string) {
-  return false;
-}
-
-bool MyString::operator>(const MyString &string) {
-  return false;
-}
-
-bool MyString::operator<(const MyString &string) {
-  return false;
-}
-
-bool MyString::operator>=(const MyString &string) {
-  return false;
-}
-
-bool MyString::operator<=(const MyString &string) {
-  return false;
-}
-
-MyString MyString::operator!() {
-  return MyString(nullptr);
-}
-
-char MyString::operator[](size_t index) {
-  return 0;
-}
-
-int32_t MyString::operator()(const char *string) {
-  return 0;
-}
-
-std::istream &operator>>(std::istream &in, MyString &string) {
-  return <#initializer#>;
-}
-
-std::ostream &operator<<(std::ostream &out, MyString &string) {
-  return <#initializer#>;
+bool MyString::operator!=(const MyString &string) const {
+  return !(string == *this);
 }
