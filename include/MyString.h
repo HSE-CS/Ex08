6460c2910f692defc86e3658189eb0ab46b76cb7
Ivan Dobryaev <ivan.dobryaev@gmail.com>
2020-12-08 00:36:15 +0300

    CPPLINT, OOOFF

--- include/MyString.h
+++ include/MyString.h
@@ -4,33 +4,33 @@
 #include <string>
 class MyString {
  private:
-   char* field;
-   int len;
+  char* field;
+  int len;
  public:
-   explicit MyString(const char* = nullptr);
-   explicit MyString(const std::string);
-   MyString(const MyString&);
-   MyString(MyString&&);
-   ~MyString();
-   int length();
-   char* get();
-   //
-   MyString operator+(const MyString&);
-   MyString operator-(const MyString&);
-   MyString operator*(int);
-   MyString& operator=(const MyString&);
-   MyString& operator=(MyString&&);
-   bool operator==(const MyString&);
-   bool operator!=(const MyString&);
-   bool operator>(const MyString&);
-   bool operator<(const MyString&);
-   bool operator>=(const MyString&);
-   bool operator<=(const MyString&);
-   MyString operator!();
-   char operator[](int);
-   int operator()(const char* other);
-   friend std::istream& operator>>(std::istream& other, MyString& str);
-   friend std::ostream& operator<<(std::ostream& other, MyString& str);
+  explicit MyString(const char* = nullptr);
+  explicit MyString(const std::string);
+  MyString(const MyString&);
+  MyString(MyString&&);
+  ~MyString();
+  int length();
+  char* get();
+
+  MyString operator+(const MyString&);
+  MyString operator-(const MyString&);
+  MyString operator*(int);
+  MyString& operator=(const MyString&);
+  MyString& operator=(MyString&&);
+  bool operator==(const MyString&);
+  bool operator!=(const MyString&);
+  bool operator>(const MyString&);
+  bool operator<(const MyString&);
+  bool operator>=(const MyString&);
+  bool operator<=(const MyString&);
+  MyString operator!();
+  char operator[](int);
+  int operator()(const char* other);
+  friend std::istream& operator>>(std::istream& other, MyString& str);
+  friend std::ostream& operator<<(std::ostream& other, MyString& str);
 };
 
 #endif  // INCLUDE_MYSTRING_H_
--- src/MyString.cpp
+++ src/MyString.cpp
@@ -4,101 +4,99 @@
 #include "MyString.h"
 
 MyString::MyString(const char* a) {
-   if (!a) {
-      len = 0;
-      char* a = reinterpret_cast<char*>(calloc(1, sizeof(char)));
-      if (!a) exit(1);
-         field = a;
-   }
-   else {
-      len = strlen(a);
-      field = strdup(a);
-   }
+  if (!a) {
+    len = 0;
+    char* a = reinterpret_cast<char*>(calloc(1, sizeof(char)));
+    if (!a) exit(1);
+      field = a;
+  }
+  else {
+    len = strlen(a);
+    field = strdup(a);
+  }
 }
 
 
 
 MyString::MyString(std::string a) {
-   len = a.length();
-   field = strdup(a.c_str());
+  len = a.length();
+  field = strdup(a.c_str());
 }
 
 MyString::MyString(const MyString& a) {
-   field = strdup(a.field);
-   len = a.len;
+  field = strdup(a.field);
+  len = a.len;
 }
 
 MyString::MyString(MyString&& a) {
-   field = strdup(a.field);
-   len = a.len;
+  field = strdup(a.field);
+  len = a.len;
 }
 
 MyString::~MyString() {
-   free(field);
+  free(field);
  }
 
- int MyString::length() {
-   return len;
+int MyString::length() {
+  return len;
  }
 
- char* MyString::get() {
-   return field;
+char* MyString::get() {
+  return field;
  }
 //
 MyString MyString::operator+(const MyString& a) {
-   char* buffer = reinterpret_cast<char*>(calloc(len + a.len + 1, sizeof(char)));
-   strcat(buffer, field);
-   strcat(buffer + len, a.field);
-   MyString result = MyString(buffer);
-   free(buffer);
-   return result;
+  char* buffer = reinterpret_cast<char*>(calloc(len + a.len + 1, sizeof(char)));
+  strcat(buffer, field);
+  strcat(buffer + len, a.field);
+  MyString result = MyString(buffer);
+  free(buffer);
+  return result;
 }
 
 MyString MyString::operator-(const MyString& a) {
-   int k = 0;
-   char* result = (char*) calloc(len + 1, sizeof(char));
-   bool letter_found = false;
-   for (int i = 0; i < len; i++) {
-   letter_found = false;
-   for (int j = 0; j < a.len; j++) {
+  int k = 0;
+  char* result = (char*) calloc(len + 1, sizeof(char));
+  bool letter_found = false;
+  for (int i = 0; i < len; i++) {
+    letter_found = false;
+    for (int j = 0; j < a.len; j++) {
       if (field[i] == a.field[j])
-         letter_found = true;
+        letter_found = true;
       }
       if (!letter_found)
       {
          result[k] = field[i];
          k++;
       }
-   }
-   MyString ans = MyString(result);
-   free(result);
-   return ans;
+     }
+  MyString ans = MyString(result);
+  free(result);
+  return ans;
 }
 
-
 MyString MyString::operator*(int repeats) {
-   char* result = (char*) calloc(len * repeats + 1, sizeof(char));
-   for(int i = 0; i < repeats; i++)
-   strcpy(result + i * repeats, field);
-
-   MyString ans = MyString(result);
-   free(result);
-   return ans;
+  char* result = (char*) calloc(len * repeats + 1, sizeof(char));
+  for(int i = 0; i < repeats; i++)
+  strcpy(result + i * repeats, field);
+  MyString ans = MyString(result);
+  free(result);
+  return ans;
 }
 
 MyString& MyString::operator=(const MyString& a) {
-   len = a.len;
-   field = strdup(a.field);
-   return *this;
+  len = a.len;
+  field = strdup(a.field);
+  return *this;
 }
 MyString& MyString::operator=(MyString&& a) {
-   len = a.len;
-   field = strdup(a.field);
-   return *this;
+  len = a.len;
+  field = strdup(a.field);
+  return *this;
 }
 
 bool MyString::operator!=(const MyString& a) {
-   return !(*this == a);
+  return !(*this == a);
 }
 
 bool MyString::operator==(const MyString& a) {
@@ -107,50 +105,50 @@ bool MyString::operator==(const MyString& a) {
 }
 
 bool MyString::operator<(const MyString& a) {
-   return strcmp(field, a.field) < 0;
+  return strcmp(field, a.field) < 0;
 }
 
 bool MyString::operator>(const MyString& a) {
-   return strcmp(field, a.field) > 0;
+  return strcmp(field, a.field) > 0;
 }
 
 bool MyString::operator>=(const MyString& a) {
-   return strcmp(field, a.field) >= 0;
+  return strcmp(field, a.field) >= 0;
 }
 
 bool MyString::operator<=(const MyString& a) {
-   return strcmp(field, a.field) <= 0;
+  return strcmp(field, a.field) <= 0;
 }
 
 MyString MyString::operator!() {
-   MyString copy(*this);
-   for (size_t i = 0; i < copy.len; ++i) {
-      if (copy.field[i] >= 'a' && copy.field[i] <= 'z') {
-         copy.field[i] -= 'a' - 'A';
-      }
-      else if (copy.field[i] >= 'A' && copy.field[i] <= 'Z') {
-         copy.field[i] += 'a' - 'A';
-      }
-   }
-   return copy;
+  MyString copy(*this);
+  for (size_t i = 0; i < copy.len; ++i) {
+    if (copy.field[i] >= 'a' && copy.field[i] <= 'z') {
+      copy.field[i] -= 'a' - 'A';
+    }
+    else if (copy.field[i] >= 'A' && copy.field[i] <= 'Z') {
+      copy.field[i] += 'a' - 'A';
+    }
+  }
+  return copy;
 }
 
 char MyString::operator[](int index) {
-   return field[index];
+  return field[index];
 }
 
 int MyString::operator()(const char* a) {
-   char* ans = strstr(field, a);
-   if (ans == NULL)
-      return -1;
-    else
-      return (field - ans);
+  char* ans = strstr(field, a);
+  if (ans == NULL)
+    return -1;
+  else
+    return (field - ans);
 }
 
 std::istream& operator>>(std::istream& in, MyString& a) {
-   return in >> a.get();
+  return in >> a.get();
 }
 
 std::ostream& operator<<(std::ostream& out, MyString& a) {
-   return out << a.get();
+  return out << a.get();
 }
