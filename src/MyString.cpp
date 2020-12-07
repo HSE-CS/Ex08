8322ede5d8c141d9472abbd5997f165f5ed0fb48
EgorBuzanov <egor4ek007@gmail.com>
2020-12-08 02:32:11 +0300

    Egor Buzanov

--- src/MyString.cpp
+++ src/MyString.cpp
@@ -47,7 +47,7 @@ char *MyString::get() const { return str; }
 MyString MyString::operator+(const MyString &second_string) {
   size_t new_len = len + second_string.length();
   char *new_str = new char[new_len + 1];
-  snprintf(new_str, len, "%s", str);
+  snprintf(new_str, len + 1, "%s", str);
   snprintf(new_str + len, second_string.length() + 1, "%s",
            second_string.get());
   return MyString(new_str);
