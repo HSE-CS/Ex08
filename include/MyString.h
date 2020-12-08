f6b3c731fcf43775bbb8cc188983cbca4c940477
Смирнов Григорий <greg.smirnov2013@yandex.ru>
2020-12-08 18:03:58 +0300

    task

--- include/MyString.h
+++ include/MyString.h
@@ -10,13 +10,30 @@ class MyString {
    char* str = nullptr;
    size_t len = 0;
  public:
-   MyString(const char* = nullptr);
-   MyString(std::string);
+   explicit MyString(const char* = nullptr);
+   explicit MyString(std::string);
    MyString(const MyString&);
    MyString(MyString&&);
    ~MyString();
    size_t length();
    char* get();
+
+   MyString operator+(const MyString&);
+   MyString operator-(const MyString&);
+   MyString operator*(int);
+   bool operator==(const MyString&) const;
+   bool operator!=(const MyString&) const;
+   bool operator>(const MyString&) const;
+   bool operator<(const MyString&) const;
+   bool operator>=(const MyString&) const;
+   bool operator<= (const MyString&) const;
+   MyString& operator=(const MyString&);
+   MyString& operator=(MyString&&);
+   MyString& operator!();
+   char& operator[](int);
+   int operator()(const char*);
+   friend std::ostream& operator>>(std::ostream&, MyString&);
+   friend std::istream& operator>>(std::istream&, MyString&);
 };
 
 #endif  // INCLUDE_MYSTRING_H_
--- src/MyString.cpp
+++ src/MyString.cpp
@@ -43,3 +43,103 @@ MyString::MyString(MyString&& for_replace) {
 char* MyString::get() { return str; }
 
 size_t MyString::length() { return len; }
+
+const char* delnullptr(const char* s) {
+  if (s == nullptr)
+    return "";
+  else
+    return s;
+}
+
+MyString MyString::operator+(const MyString& s) {
+  return MyString(std::string(delnullptr(str)) + std::string(delnullptr(s.str)));
+  //char* buf = new char[len + s.len];
+  //strcpy(buf, delnullptr(str));
+  //strcat(buf, delnullptr(s.str));
+  //MyString result(buf);
+  //delete [] buf;
+  //return result;
+  //return MyString(buf);
+}
+
+MyString MyString::operator-(const MyString& s) {
+  if (s.str == nullptr)
+    return MyString(str);
+  else if (str == nullptr)
+    return MyString();
+  int cntlen = 0;
+  for (int i = 0; i < len; i++)
+    if (strchr(s.str, str[i]) == nullptr)
+      cntlen++;
+  char* buf = new char[cntlen + 1];
+  int j = 0;
+  for (int i = 0; i < len; i++)
+    if (strchr(s.str, str[i]) == nullptr) {
+      buf[j] = str[i];
+      j++;
+    }
+  buf[j] = '\0';
+  return MyString(buf);
+}
+
+MyString MyString::operator*(int rep) {
+  std::string part(str);
+  std::string buf;
+  for (int i = 0; i < rep; i++)
+    buf += part;
+  return MyString(buf);
+}
+
+bool MyString::operator==(const MyString& s) const {
+  return !(strcmp(str, s.str));
+}
+
+bool MyString::operator!=(const MyString& s) const {
+  return (strcmp(str, s.str));
+}
+
+bool MyString::operator>(const MyString& s) const {
+  return strcmp(str, s.str) > 0;
+}
+
+bool MyString::operator<(const MyString& s) const {
+  return strcmp(str, s.str) < 0;
+}
+
+bool MyString::operator>=(const MyString& s) const {
+  return strcmp(str, s.str) >=0;
+}
+
+bool MyString::operator<=(const MyString& s) const {
+  return strcmp(str, s.str) <= 0;
+}
+
+MyString& MyString::operator=(const MyString& s) {
+  delete (this->str);
+  char* buf = new char[s.len + 1];
+  snprintf(buf, s.len + 1, "%s", s.str);
+  this->str = buf;
+  return *this;
+}
+
+MyString& MyString::operator=(MyString&& s) {
+  delete (this->str);
+  this->str = s.get();
+  s.str = nullptr;
+  return *this;
+}
+
+char& MyString::operator[](int index) {
+  if (index < 0 || len == 0 || index >= len)
+    throw "index out of range";
+  else
+    return str[index];
+}
+
+std::ostream& operator>>(std::ostream& out, MyString& new_str) {
+  return out << new_str.get();
+}
+
+std::istream& operator>>(std::istream& input, MyString& new_str) {
+  return input >> new_str.get();
+}
