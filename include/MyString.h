25479ce4eeb0f5881c0e2b5e67733a787cce7492
NikDemoShow <Nixon2001@yandex.ru>
2020-12-08 15:47:00 +0300

    add all files

---
+++ include/MyString.h
@@ -0,0 +1,38 @@
+// Copyright NikDemoShow 2020
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#ifndef INCLUDE_MYSTRING_H_
+#define INCLUDE_MYSTRING_H_
+class MyString {
+ private:
+	 char* str;
+	 size_t len;
+ public:
+  explicit MyString(const char* new_str = nullptr);
+  explicit MyString(std::string);
+  MyString(const MyString&);
+  MyString(MyString&&);
+  ~MyString();
+  size_t length() const;
+  char* get() const;
+
+  MyString operator+(const MyString&);
+  MyString operator-(const MyString&);
+  MyString operator*(const size_t);
+  MyString& operator=(const MyString&);
+  MyString& operator=(MyString&&);
+  bool operator==(const MyString&);
+  bool operator!=(const MyString&);
+  bool operator>(const MyString&);
+  bool operator<(const MyString&);
+  bool operator>=(const MyString&);
+  bool operator<=(const MyString&);
+  MyString operator!();
+  char& operator[](const size_t);
+  int operator()(const char*);
+  friend std::istream& operator>>(std::istream& stream, MyString s);
+  friend std::ostream& operator<<(std::ostream& stream, MyString s);
+};
+#endif
