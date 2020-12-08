47528e3392069a825f3d3f377d2599ddb08f6b3b
Khoroshavina Ekaterina <63115848+KatrinLion14@users.noreply.github.com>
2020-12-08 15:25:27 +0300

    Update MyString.h

--- include/MyString.h
+++ include/MyString.h
@@ -6,33 +6,34 @@
 
 class MyString {
  private:
-     char* string;
-     int len;
+    char* string;
+    int len;
 
  public:
-     explicit MyString(const char* str = nullptr);
-     explicit MyString(const std::string& str);
-     MyString(const MyString& str);
-     MyString(MyString&& str) noexcept;
-     ~MyString();
-     int length() const;
-     char* get() const;
-     MyString operator+(const MyString& str);
-     MyString operator-(const MyString& str);
-     MyString operator*(int a);
-     MyString& operator=(const MyString& str);
-     MyString& operator=(MyString&& str);
-     bool operator==(const MyString& str);
-     bool operator!=(const MyString& str);
-     bool operator>(const MyString& str);
-     bool operator<(const MyString& str);
-     bool operator>=(const MyString& str);
-     bool operator<=(const MyString& str);
-     MyString operator!();
-     char operator[](int i);
-     int operator()(const char* str);
-     friend std::istream& operator>>(std::istream& in, MyString& str);
-     friend std::ostream& operator<<(std::ostream& out, MyString& str);
+    MyString();
+    explicit MyString(const char* str);
+    explicit MyString(const std::string& str);
+    MyString(const MyString& str);
+    MyString(MyString&& str) noexcept;
+    ~MyString();
+    int length() const;
+    char* get() const;
+    MyString operator+(const MyString& str);
+    MyString operator-(const MyString& str);
+    MyString operator*(int a);
+    MyString& operator=(const MyString& str);
+    MyString& operator=(MyString&& str);
+    bool operator==(const MyString& str);
+    bool operator!=(const MyString& str);
+    bool operator>(const MyString& str);
+    bool operator<(const MyString& str);
+    bool operator>=(const MyString& str);
+    bool operator<=(const MyString& str);
+    MyString operator!();
+    char operator[](int i);
+    int operator()(const char* str);
+    friend std::istream& operator>>(std::istream& in, MyString& str);
+    friend std::ostream& operator<<(std::ostream& out, MyString& str);
 };
 
 #endif  // INCLUDE_MYSTRING_H_
