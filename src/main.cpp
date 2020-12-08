156e93f22804ee5855ac553be18937ce945719e6
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2020-12-08 18:08:09 +0300

    writing scripts is boring

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,11 @@
 #include "../include/MyString.h"
 
 int main() {
+  MyString s("hello!");
+  MyString k = s * 2;
+  std::cout << k.get();
+  k = k + k;
+  std::cout << k;
+  std::cout << (k != s);
   return 0;
 }
