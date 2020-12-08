5d15c506725836c3ec4fb9320fe872b58bb3023e
Дима булатов <dabulatov@edu.hse.ru>
2020-12-08 23:39:32 +0300

    str.h

--- include/MyString.h
+++ include/MyString.h
@@ -15,28 +15,28 @@ class MyString {
      size_t len;
  public:
 	 explicit MyString(const char* s = nullptr);
-	 explicit MyString(string);
-	 MyString(const MyString&);
-	 MyString(MyString&&);
+	 explicit MyString(string s);
+	 MyString(const MyString& s);
+	 MyString(MyString&& s);
 	 ~MyString();
 	 size_t length();
 	 char* get();
-	 MyString operator+(const MyString&);
-	 MyString operator-(const MyString&);
-	 MyString operator*(size_t);
-	 MyString& operator=(const MyString&);
-	 MyString& operator=(MyString&&);
-	 bool operator==(const MyString&);
-	 bool operator!=(const MyString&);
-	 bool operator>(const MyString&);
-	 bool operator<(const MyString&);
-	 bool operator>=(const MyString&);
-	 bool operator<=(const MyString&);
+	 MyString operator+(const MyString& s);
+	 MyString operator-(const MyString& s);
+	 MyString operator*(size_t n);
+	 MyString& operator=(const MyString& s);
+	 MyString& operator=(MyString&& s);
+	 bool operator==(const MyString& s);
+	 bool operator!=(const MyString& s);
+	 bool operator>(const MyString& s);
+	 bool operator<(const MyString& s);
+	 bool operator>=(const MyString& s);
+	 bool operator<=(const MyString& s);
 	 MyString operator!();
-	 char operator[](size_t);
-	 int operator()(const char*);
+	 char operator[](size_t n);
+	 int operator()(const char* s);
 	 friend ostream& operator>>(ostream& os, MyString& str);
- 	 friend istream& operator>>(istream& is, MyString& str);
+	 friend istream& operator>>(istream& is, MyString& str);
 };
 
 #endif  // INCLUDE_MYSTRING_H_
