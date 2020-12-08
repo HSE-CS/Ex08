aba12146f2d91a8df7424d45c64b80b377b3cac3
Osmanov Islam <osmangov@yandex.ru>
2020-12-08 19:43:32 +0300

    Osmanov v0.2

---
+++ src/MyString.cpp
@@ -0,0 +1,160 @@
+//Copyright 2020 OsmanovIslam
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include "MyString.h"
+
+
+int MyString::length(){
+	return strlen(str);
+}
+
+char* MyString::get() const {
+	return str;
+}
+
+MyString::MyString(const char* StrArr) {
+	if (StrArr == nullptr) {
+		str = new char[1];
+	}
+	else {
+		char* bufStrArray = new char[strlen(StrArr) + 1];
+		snprintf(str, strlen(StrArr) + 1, "%s", StrArr);
+		str = bufStrArray;
+	}
+}
+
+MyString::MyString(std::string only_str) {
+	int size = only_str.size() + 1;
+	str = new char[size];
+	snprintf(str, size, "%s", only_str.c_str());
+}
+
+MyString::MyString(MyString&& only_str) noexcept{
+	str = only_str.str;
+	only_str.str = nullptr;
+}
+
+MyString::MyString(const MyString& val_str) {
+	int size = strlen(val_str.str) + 1;
+	str = new char(size);
+	snprintf(str, size, "%s", val_str.str);
+
+}
+
+MyString::~MyString() {
+	delete[] str;
+}
+
+MyString MyString::operator+(const MyString& other_string) {
+	int size1 = strlen(str);
+	int size2 = strlen(other_string.str);
+	std::string bufStr;
+	bufStr = std::string(str) + std::string(other_string.str);
+	return MyString(bufStr);
+}
+
+MyString& MyString::operator=(const MyString& other_string) {
+	str = _strdup(other_string.str);
+	return *this;
+}
+
+MyString& MyString::operator=(MyString&& other_string) noexcept {
+	str = other_string.str;
+	other_string = nullptr;
+	return *this;
+}
+
+MyString MyString::operator-(const MyString& other_string) {
+	char* bufStr = new char(length());
+	int size = 0;
+	bool f;
+	for (int i = 0; i < length(); i++){
+		f = false;
+		for (int k = 0; k < strlen(other_string.str); k++){
+			if (str[i] == other_string.str[k])
+				f = true;
+		}
+		if (f == false) {
+			bufStr[size] = str[i];
+			size++;
+		}
+	}
+	return MyString(bufStr);
+}
+
+MyString MyString::operator*(const int num) {
+	int bufSize = (length() * num) + 1;
+	char* bufStr = new char[bufSize];
+	int act_len = 0;
+	for (int i = 0; i < num; i++){
+		for (int k = 0; k < length(); k++) {
+			bufStr[act_len] = str[k];
+			act_len++;
+		}
+	}
+	bufStr[act_len] = '\0';
+	return MyString(bufStr);
+}
+
+bool MyString::operator==(const MyString& string) {
+	return !strcmp(str, string.str);
+}
+
+bool MyString::operator!=(const MyString& string) {
+	return strcmp(str, string.str);
+}
+
+bool MyString::operator<(const MyString& string) {
+	return (strcmp(str, string.get()) == -1);
+}
+
+bool MyString::operator>(const MyString& string) {
+	return (strcmp(str, string.get()) == 1);
+}
+
+bool MyString::operator>=(const MyString& string)
+{
+	return (strcmp(str, string.get()) != -1);
+}
+
+bool MyString::operator<=(const MyString& string)
+{
+	return strcmp(str, string.get()) != 1;
+}
+
+MyString MyString::operator!() {
+	int size = length();
+	char* bufStr = new char[size + 1];
+	for (int k = 0; k < size; k++) {
+		if (str[k] > 64 && str[k] < 91) {
+			bufStr[k] = tolower(str[k]);
+		}
+		if (str[k] >= 97 && str[k] <= 122) {
+			bufStr[k] = toupper(str[k]);
+		}
+	}
+
+	return MyString(bufStr);
+}
+
+int MyString::operator()(const char* string) {
+	std::string find_pod_str = str;
+	return find_pod_str.find(string);
+}
+char MyString::operator[](unsigned int num) const {
+	return str[num];
+}
+
+std::istream& operator>>(std::istream& input, MyString& only_string) {
+	int size = input.gcount() + 1;
+    std::string bufString;
+	std::getline(input, bufString);
+	only_string = MyString(bufString);
+	return input;
+}
+
+std::ostream& operator<<(std::ostream& output, MyString& only_string) {
+	return output << only_string.get();
+}
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include "MyString.h"
+
+int main()
+{
+    MyString s1("Hello,");
+    MyString s2("world!");
+    MyString s3;
+    s3 = s1 + s2;
+    std::cout << s3;
+    std::cout << s3("ll");   // 2
+    std::cout << s3("mama"); // -1
+}
