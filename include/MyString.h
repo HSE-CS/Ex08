6e742fdf189fa7df5fdc3cd115c0d4539f9ea686
Alyona Uskova <aluskova01@yandex.ru>
2020-12-08 14:57:59 +0300

    Task 1

---
+++ include/MyString.h
@@ -0,0 +1,38 @@
+// Copyright 2020 Uskova
+#ifndef INCLUDE_MYSTRING_H_
+#define INCLUDE_MYSTRING_H_
+
+#include <string>
+#include <string.h>
+#include <iostream>
+
+class MyString {
+ private:
+     char* string;
+ public:
+    explicit MyString(const char* = nullptr);
+    explicit MyString(std::string);
+    MyString(const MyString&);
+    MyString(MyString&&);
+    ~MyString();
+    size_t length() const;
+    char* get() const;
+    MyString operator+(const MyString&);
+    MyString operator-(const MyString&);
+    MyString operator*(int);
+    MyString& operator=(const MyString&);
+    MyString& operator=(MyString&&);
+    bool operator==(const MyString&);
+    bool operator!=(const MyString&);
+    bool operator>(const MyString&);
+    bool operator<(const MyString&);
+    bool operator>=(const MyString&);
+    bool operator<=(const MyString&);
+    MyString operator!();
+    char operator[](size_t) const;
+    int operator()(const char*);
+    friend std::ostream& operator<<(std::ostream&, MyString&);
+    friend std::istream& operator>>(std::istream&, MyString&);
+};
+
+#endif    // INCLUDE_MYSTRING_H_
