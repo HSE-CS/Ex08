57542afb855824f27981d77a807c9d190dc099c2
natasha <igum.natasha@gmail.com>
2020-12-08 14:25:28 +0300

    Fix bugs

--- src/main.cpp
+++ src/main.cpp
@@ -11,26 +11,6 @@ int main() {
     std::cout << s3;
     std::cout << s3("ll");  // 2
     std::cout << s3("mama");  // -1
-    MyString a;
-    MyString a1("hello");
-    MyString a2("123");
-    MyString b(a2);
-    MyString a3("123");
-    MyString b(a3);
-    MyString c;
-    c = a + b;
-    MyString a4("123");
-    MyString b3;
-    MyString c1;
-    c1 = a4 + b3;
-    MyString a5(std::string("Hello"));
-    MyString a6("123456789");
-    MyString b4("2468");
-    MyString c2;
-    c2 = a6 - b4;
-    MyString a7("123");
-    int b5 = 3;
-    MyString c3;
     MyString a8("454545");
     std::cout<< a8("33");
 }
