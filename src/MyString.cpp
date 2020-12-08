0de616f11c4503698f6f2b689ec6390a2fd00170
Alyona Uskova <aluskova01@yandex.ru>
2020-12-08 15:31:43 +0300

    fix

--- src/MyString.cpp
+++ src/MyString.cpp
@@ -66,7 +66,7 @@ MyString MyString::operator-(const MyString& str) {
 MyString MyString::operator*(int num) {
     std::string str;
     for (int i = 0; i < num; i++) {
-        str += this->get();
+        str += this->string;
     }
     return MyString(str);
 }
