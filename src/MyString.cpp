3c1ace0cf9e26d4b769fbadaf869181ce2d87803
Андрей Рогов <adrogov@edu.hse.ru>
2020-12-08 17:07:31 +0300

    123

---
+++ src/MyString.cpp
@@ -0,0 +1,148 @@
+#include "MyString.h"
+#include <string>
+#include <cstring>
+#include <iostream>
+
+char* MyString::get() const {
+    return my_string;
+}
+
+unsigned int MyString::length() const {
+    return strlen(my_string);
+}
+
+MyString::MyString(const char* initial_string) {
+    if (initial_string == nullptr) {
+        my_string = new char[1];
+    } else {
+        unsigned int len = strlen(initial_string) + 1;
+        my_string = new char[len];
+        snprintf(my_string, len, "%s", initial_string);
+    }
+}
+
+MyString::MyString(std::string initial_string) {
+    unsigned int len = initial_string.length() + 1;
+    my_string = new char[len];
+    snprintf(my_string, len, "%s", initial_string.c_str());
+}
+
+MyString::MyString(const MyString& initial_string) {
+    unsigned int str_len = initial_string.length() + 1;
+    my_string = new char[str_len];
+    snprintf(my_string, str_len, "%s", initial_string.get());
+}
+
+MyString::MyString(MyString&& initial_string) noexcept {
+    my_string = initial_string.my_string;
+    initial_string.my_string = nullptr;
+}
+
+MyString::~MyString() {
+    delete my_string;
+}
+
+
+MyString& MyString::operator=(const MyString& second_string) {
+    if (my_string != nullptr)
+        delete my_string;
+    unsigned int str_len = second_string.length() + 1;
+    my_string = new char[str_len];
+    snprintf(my_string, str_len, "%s", second_string.get());
+    return *this;
+}
+
+MyString& MyString::operator=(MyString&& second_string) noexcept {
+    if (my_string != nullptr)
+        delete my_string;
+    my_string = second_string.get();
+    second_string.my_string = nullptr;
+    return *this;
+}
+
+MyString MyString::operator+(const MyString& second_string) {
+    unsigned int str_len1 = length();
+    unsigned int str_len2 = second_string.length();
+    char* new_str = new char[str_len1 + str_len2 + 1];
+    snprintf(new_str, str_len1 + 1, "%s", my_string);
+    snprintf(new_str + str_len1, str_len2 + 1, "%s", second_string.my_string);
+    return MyString(new_str);
+}
+
+MyString MyString::operator-(const MyString& second_string) {
+    unsigned int str_len1 = length();
+    unsigned int new_str_len = 0;
+    std::string str2 = second_string.get();
+    char* new_str = new char[str_len1 + 1];
+    for (unsigned int i = 0; i < str_len1; i++)
+        if (!strchr(str2.c_str(), my_string[i]))
+            new_str[new_str_len++] = my_string[i];
+    new_str[new_str_len] = '\0';
+    return MyString(new_str);
+}
+
+MyString MyString::operator*(unsigned int number) {
+    std::string new_str;
+    unsigned int len = length();
+    for (unsigned int i = 0; i < len; i++)
+        new_str.append(get());
+    return MyString(new_str);
+}
+
+MyString& MyString::operator!() {
+    unsigned int str_len = length();
+    for (unsigned int i = 0; i < str_len; i++)
+        if (my_string[i] >= 65 && my_string[i] <= 90)
+            my_string[i] = my_string[i] + 32;
+        else if (my_string[i] >= 97 && my_string[i] <= 122)
+            my_string[i] = my_string[i] - 32;
+    return *this;
+}
+
+
+bool MyString::operator==(const MyString& second_string) const {
+    return !strcmp(my_string, second_string.get());
+}
+
+bool MyString::operator!=(const MyString& second_string) const {
+    return strcmp(my_string, second_string.get());
+}
+
+bool MyString::operator>(const MyString& second_string) const {
+    return strcmp(my_string, second_string.get()) > 0;
+}
+
+bool MyString::operator<(const MyString& second_string) const {
+    return strcmp(my_string, second_string.get()) < 0;
+}
+bool MyString::operator>=(const MyString& second_string) const {
+    return strcmp(my_string, second_string.get()) >= 0;
+}
+
+bool MyString::operator<=(const MyString& second_string) const {
+    return strcmp(my_string, second_string.get()) <= 0;
+}
+
+
+char& MyString::operator[](unsigned int index) const {
+    if (index >= length()) throw "Number out of range";
+    return my_string[index];
+}
+
+int MyString::operator()(const char* target_string) {
+    std::string str = get();
+    return str.find(target_string);
+}
+
+
+std::ostream& operator<<(std::ostream& os, MyString& str) {
+    return os << str.get();
+}
+
+std::istream& operator>>(std::istream& is, MyString& str) {
+    unsigned int len = is.gcount() + 1;
+    std::string new_str;
+    std::getline(is, new_str);
+    str = MyString(new_str);
+    return is;
+}
---
+++ src/main.cpp
@@ -0,0 +1,57 @@
+#include "MyString.h"
+#include<stdio.h>
+#include <string>
+#include <cstring>
+#include <iostream>
+
+int main(){
+	MyString str("Hello");
+	printf("STR: %s\n", str.get());
+	std::cout << "Str lenght: " << str.length() << std::endl;
+	std::string str1 = "Hello World!";
+	MyString str2(str1);
+	printf("STR2: %s\n", str2.get());
+	std::cout << "Str lenght: " << str2.length() << std::endl;
+	MyString str3(str2);
+	printf("STR3: %s\n", str3.get());
+	std::cout << "Str lenght: " << str3.length() << std::endl;
+	MyString str4 = str2;
+	printf("%s\n", str4.get());
+	std::cout << "Str lenght: " << str4.length() << std::endl;
+	MyString str5 = str2 + str3;
+	printf("STR2 + STR3: %s\n", str5.get());
+	std::cout << "Str lenght: " << str5.length() << std::endl;
+	MyString str6 = str2 - str;
+	printf("STR2 - STR: %s\n", str6.get());
+	std::cout << "Str lenght: " << str6.length() << std::endl;
+	MyString str7 = str * 5;
+	printf("STR*5: %s\n", str7.get());
+	std::cout << "Str lenght: " << str7.length() << std::endl;
+	MyString str8 = !str7;
+	printf("!STR*5: %s\n", str8.get());
+	std::cout << "Str lenght: " << str8.length() << std::endl;
+	bool result = str == str;
+	std::cout << "STR == STR: " << result << std::endl;
+	result = str != str;
+	std::cout << "STR != STR: " << result << std::endl;
+	result = str == str2;
+	std::cout << "STR == STR2: " << result << std::endl;
+	result = str2 > str;
+	std::cout << "STR2 > STR: " << result << std::endl;
+	result = str > str2;
+	std::cout << "STR > STR2: " << result << std::endl;
+	result = str <= str;
+	std::cout << "STR <= STR: " << result << std::endl;
+	result = str2 >= str;
+	std::cout << "STR2 >= STR: " << result << std::endl;
+	std::cout << "STR2[1]: " << str2[1] << std::endl;
+	std::cout << "Find Hello in STR2: " << str2("Hello") << std::endl;
+	std::cout << "Find hello in STR2: " << str2("hello") << std::endl;
+	std::cout << "STR2 in COUT: " << str2 << std::endl;
+	MyString a("123");
+	int b = 3;
+	MyString c;
+	c = a * b;
+	std::cout << c.get() << std::endl;
+	return 0;
+}
