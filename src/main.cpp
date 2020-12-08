822a03f061ce19e0075c7de6173282e2ceeb9353
Pasha Emshanov <63115710+Dev-will-work@users.noreply.github.com>
2020-12-08 17:27:55 +0300

    style fixes in main

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,6 @@ int main() {
   MyString s3;
   s3 = s1 + s2;
   std::cout << s3;
-  std::cout << s3("ll");   // 2
-  std::cout << s3("mama"); // -1
+  std::cout << s3("ll");  // 2
+  std::cout << s3("mama");  // -1
 }
