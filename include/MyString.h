0b6af8d15e2632d71123a1b584e7af96c086ea6e
Pasha Emshanov <63115710+Dev-will-work@users.noreply.github.com>
2020-12-08 16:57:58 +0300

    Fixed declarations of copy and move constructors and operators in header

--- include/MyString.h
+++ include/MyString.h
@@ -5,29 +5,29 @@
 class MyString
 {
 private:
-	char* string;
+	char* string = nullptr;
 public:
 	MyString(const char* str = nullptr); // -конструктор с одним параметром(есть значение по - умолчанию nullptr).
 	MyString(std::string); // -конструктор с одним параметром.
-	MyString(const MyString&); // -конструктор копирования.
-	MyString(MyString&&); // -конструктор переноса.
+	MyString(const MyString& str); // -конструктор копирования.
+	MyString(MyString&& str) : string(str.string) { str.string = nullptr; }; // -конструктор переноса.
 	~MyString(); // -деструктор.
 	size_t length(); // -количество символов(длина строки).
 	char* get(); // -возвращение указателя на данные(тип char*).
-	MyString operator+(); // -сложение(конкатенация двух строк).
-	MyString operator-(); // -вычитание(из первой строки удаляются все символы, присутствующие во второй строке).
-	MyString operator*(); // -умножение на целое число(строка повторяется заданное число раз).
+	MyString operator+(MyString); // -сложение(конкатенация двух строк).
+	MyString operator-(MyString); // -вычитание(из первой строки удаляются все символы, присутствующие во второй строке).
+	MyString operator*(int); // -умножение на целое число(строка повторяется заданное число раз).
 	MyString& operator=(const MyString&); // -копирующее присваивание.
-	MyString& operator=(MyString); // -перемещающее присваивание.
-	MyString operator==(MyString); // -сравнение на равенство.
-	MyString operator!=(MyString); // -сравнение на неравенство.
-	MyString operator>(MyString); // -лексографическое сравнение .
-	MyString operator<(MyString); // -лексографическое сравнение.
-	MyString operator>=(MyString); // -лексографическое сравнение.
-	MyString operator<=(MyString); // -лексографическое сравнение.
+	MyString& operator=(MyString&&); // -перемещающее присваивание.
+	bool operator==(MyString); // -сравнение на равенство.
+	bool operator!=(MyString); // -сравнение на неравенство.
+	bool operator>(MyString); // -лексографическое сравнение .
+	bool operator<(MyString); // -лексографическое сравнение.
+	bool operator>=(MyString); // -лексографическое сравнение.
+	bool operator<=(MyString); // -лексографическое сравнение.
 	MyString operator!(); // - у латинских букв меняется регистр.
 	char operator[](int); // - доступ к символу по индексу.
-	MyString operator()(MyString); // - поиск подстроки.
+	int operator()(MyString); // - поиск подстроки.
 	friend std::ostream& operator<<(std::ostream& out, MyString& str); // -чтение из потока.
 	friend std::istream& operator>>(std::istream& in, MyString& str); // -запись в поток.
 };
