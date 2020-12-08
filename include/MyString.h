e775ae811c326d1792219ee6fb25e41c546c5ebb
Дима булатов <dabulatov@edu.hse.ru>
2020-12-09 01:39:05 +0300

    ....

--- include/MyString.h
+++ include/MyString.h
@@ -6,36 +6,38 @@
 #include <string>
 #include <numeric>
 #include <algorithm>
+#include <string.h>
 
 class MyString {
-private:
-	char* stroka;
-	int len;
+ private:
+  char* stroka;
+  int len;
 
-public:
-	explicit MyString(const char* = nullptr);
-	explicit MyString(std::string s);
-	MyString(const MyString&);
-	MyString(MyString&&);
-	~MyString();
-	int length();
-	char* get();
-	MyString operator+(const MyString& s);
-	MyString operator-(const MyString& s);
-	MyString operator*(const int n);
-	MyString& operator=(const MyString& s);
-	MyString& operator=(MyString&& s);
-	bool operator==(const MyString& s);
-	bool operator!=(const MyString& s);
-	bool operator>(const MyString& s);
-	bool operator<(const MyString& s);
-	bool operator>=(const MyString& s);
-	bool operator<=(const MyString& s);
-	MyString operator!();
-	char operator[](int n);
-	int operator()(const char*);
-	friend std::ostream& operator>>(std::ostream& os, MyString& s);
-	friend std::istream& operator>>(std::istream& is, MyString& s);
+ public:
+  explicit MyString(const char* = nullptr);
+  explicit MyString(std::string s);
+  MyString(const MyString&);
+  MyString(MyString&&);
+  ~MyString();
+  int length();
+  char* get();
+  MyString operator+(const MyString& s);
+  MyString operator-(const MyString& s);
+  MyString operator*(const int n);
+  MyString& operator=(const MyString& s);
+  MyString& operator=(MyString&& s);
+  bool operator==(const MyString& s);
+  bool operator!=(const MyString& s);
+  bool operator>(const MyString& s);
+  bool operator<(const MyString& s);
+  bool operator>=(const MyString& s);
+  bool operator<=(const MyString& s);
+  MyString operator!();
+  char operator[](int n);
+  int operator()(const char*);
+  friend std::ostream& operator>>(std::ostream& os, MyString& s);
+  friend std::istream& operator>>(std::istream& is, MyString& s);
+
 };
 
 #endif  // INCLUDE_MYSTRING_H_
