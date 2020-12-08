db922f31f426d3c31e62a162f1cd5fd36793ad99
Your Name <olga.frolova.2001@bk.ru>
2020-12-08 11:22:04 +0300

    working

--- src/MyString.cpp
+++ src/MyString.cpp
@@ -1,1 +1,56 @@
 // Copyright 2020 FOM
+#include "MyString.h"
+
+MyString::MyString() {
+    string = nullptr;
+    lenght = 0;
+}
+
+MyString::MyString(const char* str) {
+    if (str != nullptr) {
+        lenght = strlen(str);
+        str = new char[lenght + 1];
+        snprintf(string, lenght + 1, "%s", str);
+    }
+    else {
+        string = nullptr;
+        lenght = 0;
+    }
+}
+
+MyString::MyString(std::string str) {
+    lenght = str.size();
+    string = new char[lenght + 1];
+    snprintf(string, lenght + 1, "%s", str.c_str());
+}
+
+MyString::MyString(const MyString& str) {
+    lenght = str.lenght;
+    string = new char[str.lenght + 1];
+    for (int i = 0; i < lenght; i++) {
+        string[i] = str.string[i];
+    }
+    string[lenght] = '\0';
+}
+
+MyString::MyString(MyString&& str) {
+    lenght = str.lenght;
+    string = new char[str.lenght + 1];
+    for (int i = 0; i < lenght; i++) {
+        string[i] = str.string[i];
+    }
+    string[lenght] = '\0';
+    str.~MyString();
+}
+
+MyString::~MyString() {
+    delete[]string;
+}
+
+unsigned int MyString::length() {
+    return strlen(this->string);
+}
+
+char* MyString::get() {
+    return string;
+}
