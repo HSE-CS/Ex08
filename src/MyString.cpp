99345bdc99650c21cd3bc75cef5a09825fbd86a8
Andrey <bakurskii2001@gmail.com>
2020-12-08 16:13:12 +0300

    fix bugs

--- src/MyString.cpp
+++ src/MyString.cpp
@@ -3,25 +3,29 @@
 
 MyString::MyString(const char* c_str) {
   if (c_str == nullptr) {
-    this->array = new char[0];
+    this->array = new char[1];
+	memset(this->array, 0, 1);
     this->size = 0;
     return;
   }
 
   this->size = strlen(c_str);
   this->array = new char[this->size + 1];
+  memset(this->array, 0, this->size + 1);
   memcpy(this->array, c_str, this->size);
 }
 
 MyString::MyString(std::string str) {
   this->size = str.length();
   this->array = new char[this->size + 1];
+  memset(this->array, 0, this->size + 1);
   memcpy(this->array, str.c_str(), this->size);
 }
 
 MyString::MyString(const MyString& str) {
   this->size = str.length();
-  this->array = new char[this->size];
+  this->array = new char[this->size + 1];
+  memset(this->array, 0, this->size + 1);
   memcpy(this->array, str.get(), this->size);
 }
 
@@ -81,6 +85,7 @@ MyString& MyString::operator=(const MyString& str) {
 
   this->size =  str.length();
   this->array = new char[this->size + 1];
+  memset(this->array, 0, this->size + 1);
   memcpy(this->array, str.get(), this->size);
 
   return *this;
