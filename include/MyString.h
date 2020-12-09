a97ebaffb7c4128d170fd7e1538c0f59be30ef8a
DenisKabanov <dskabanov@edu.hse.ru>
2020-12-09 23:06:17 +0300

    b71

---
+++ include/MyString.h
@@ -0,0 +1,44 @@
+// Copyright 08.12.20 DenisKabanov
+
+#ifndef INCLUDE_MYSTRING_H_
+#define INCLUDE_MYSTRING_H_
+
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+class MyString {
+ private:
+    char* string;
+	int lenofstr;
+ public:
+    explicit MyString(const char* string = nullptr);
+    explicit MyString(std::string string);
+    MyString(const MyString& prevstring);
+    MyString(MyString&& prevstring);
+    ~MyString();
+    int length();
+    char* get();
+    MyString operator+ (const MyString& astring);
+    MyString operator- (const MyString& mstring);
+    MyString operator* (const int number);
+    MyString& operator=(const MyString& prevstring);
+    MyString& operator=(MyString&& prevstring);
+    bool operator== (const MyString& string);
+    bool operator!= (const MyString& string);
+    bool operator> (const MyString& string);
+    bool operator< (const MyString& string);
+    bool operator>= (const MyString& string);
+    bool operator<= (const MyString& string);
+    MyString operator! ();
+    char operator[] (const int number);
+    int operator() (const char* string);
+    friend std::istream& operator>>(std::istream& is, MyString& string) {
+        return is >> string.get();
+    }
+    friend std::ostream& operator<<(std::ostream& os, MyString& string) {
+        return os << string.get();
+    }
+};
+
+#endif  // INCLUDE_MYSTRING_H_
---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include "MyString.h"
+#include <string>
+
+int main()
+{
+    MyString s1("Hello,");
+    MyString s2("world!");
+    MyString s3;
+    s3 = s1 + s2;
+    std::cout << s3 << std::endl;
+    MyString s4 = s3 * 3;
+    std::cout << s4 << std::endl;
+    MyString s5("l");
+    MyString s6 = s3 - s5;
+    std::cout << s6 << std::endl;
+    std::cout << s1.get() << "  " << s1.length() << std::endl;
+    MyString s7("Hello");
+    std::cout << " s7 == s1? " << (s7 == s1) << std::endl;
+    MyString s8(",");
+    s7 = s7 + s8;
+    std::cout << " s7 == s1? " << (s7 == s1) << std::endl;
+    MyString s9("Git"), s10("Hub");
+    std::cout << " s9 > s10? " << (s9 > s10) << std::endl;
+    std::cout << " s9 < s10? " << (s9 < s10) << std::endl;
+    std::cout << "Second letter of s10: " << s10[1] << std::endl;
+    MyString s11=s9+s10;
+    std::cout << "substring from: " << s11("tHub") << std::endl;
+    MyString s12 = !(s11);
+    std::cout << "Reverse " << s12 << std::endl;
+    return 0;
+}
