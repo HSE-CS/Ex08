172b7d8e07720edcf21ea88f9eace29f8ba40bdc
MILA <milalupehina@gmail.com>
2020-12-09 10:09:07 +0300

    fourth .cpp

--- src/main.cpp
+++ src/main.cpp
@@ -14,32 +14,31 @@
     //std::cout << s3("mama"); // -1
 }*/
 
-
-
 int main() {
+    /*
     // test1 +++
-    /* MyString a;
-    std::cout << a.length() << "    //0";
-    */
+    MyString a1;
+    std::cout << a1.length() << "    //0";
+
 
     // test2 +++
-    /* MyString a("hello");
-    EXPECT_EQ(5, a.length());
-    std::cout << a.length() << "    //5";
-    */
+    MyString a2("hello");
+    std::cout << a2.length() << "    //5";
+
 
     // test3 +++
-    /* MyString a("123");
-    MyString b(a);
-    std::cout << b.get() << "    //123";
-    */
+    MyString a3("123");
+    MyString b3(a);
+    std::cout << b3.get() << "    //123";
+
 
     //test4 +++
-    /* MyString a("123");
-    MyString b(a);
-    MyString c;
-    c = a + b;
-    std::cout << c.get() << "    //123123";
+    MyString a4("123");
+    MyString b4(a4);
+    MyString c4;
+    c4 = a4 + b4;
+    std::cout << c4.get() << "    //123123";
+
     */
 
     //test5 ---
@@ -49,16 +48,17 @@ int main() {
     c5 = a5 + b5;
     std::cout << c5.get() << std::endl;
 
+    /*
     // test6 +++
-    /* MyString a(std::string("Hello"));
-    std::cout << a.get() << "    //Hello";
-    */
+    MyString a16(std::string("Hello"));
+    std::cout << a16.get() << "    //Hello";
+
 
     // test7 +++
-    /* MyString a("123456789");
-    MyString b("2468");
-    MyString c;
-    c = a - b;
+    MyString a17("123456789");
+    MyString b17("2468");
+    MyString c17;
+    c17 = a17 - b17;
     std::cout << c.get() << "    //13579";
     */
 
@@ -69,62 +69,65 @@ int main() {
     c8 = a8 * b8;
     std::cout << c8.get() << std::endl;
 
+    /*
     // test9 +++
-    /* MyString a("123");
-    MyString b("123");
-    a == b ? std::cout << 1 << "   //1": std::cout << 0;
-    */
+     MyString a9("123");
+    MyString b9("123");
+    a9 == b9 ? std::cout << 1 << "   //1": std::cout << 0;
+
 
     // test10 +++
-    /* MyString a("123");
-    MyString b("1234");
-    a == b ? std::cout << 1 : std::cout << 0 << "    //0";
-    */
+     MyString a10("123");
+    MyString b10("1234");
+    a10 == b10 ? std::cout << 1 : std::cout << 0 << "    //0";
+
 
     // test11 +++
-    /* MyString a("123");
-    MyString b("1234");
-    a != b ? std::cout << 1 << "    //1" : std::cout << 0;
-    */
+     MyString a11("123");
+    MyString b11("1234");
+    a11 != b11 ? std::cout << 1 << "    //1" : std::cout << 0;
+
 
     // test12 +++
-    /* MyString a("123");
-    MyString b("1234");
-    b >= a ? std::cout << 1 << "    //1" : std::cout << 0;
-    */
+     MyString a12("123");
+    MyString b12("1234");
+    b12 >= a12 ? std::cout << 1 << "    //1" : std::cout << 0;
+
 
     // test13 +++
-    /* MyString a("45");
-    MyString b("1234");
-    b <= a ? std::cout << 1 << "    //1" : std::cout << 0;
-    */
+     MyString a13("45");
+    MyString b13("1234");
+    b13 <= a13 ? std::cout << 1 << "    //1" : std::cout << 0;
+
 
     // test14 +++
-    /* MyString a("abc");
-    MyString b;
-    b = !a;
-    std::cout << b.get() << "   //ABC";
-    */
+     MyString a14("abc");
+    MyString b14;
+    b14 = !a14;
+    std::cout << b14.get() << "   //ABC";
+
 
     // test15 +++
-    /* MyString a("abc123");
-    MyString b;
-    b = !a;
-    std::cout << b.get() << "   //ABC123";
-    */
+     MyString a15("abc123");
+    MyString b15;
+    b15 = !a15;
+    std::cout << b15.get() << "   //ABC123";
+
 
     // test16 +++
-    /* MyString c("45");
-    char d = '5';
-    std::cout << c[1] << "   //b";
-    */
+     MyString c16("45");
+    char d16 = '5';
+    std::cout << c16[1] << "   //b";
+
 
     // test17 +++
-    /* MyString b("454545");
-    std::cout << b("45") << "    //0" << std::endl;
-    */
+     MyString b17("454545");
+    std::cout << b17("45") << "    //0" << std::endl;
 
-    //test18 ---
+
+    //test18 +++
     MyString a16("454545");
     std::cout << a16("33") << "    //-1" << std::endl;
+
+    */
 }
