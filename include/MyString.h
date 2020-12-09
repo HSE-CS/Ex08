8ed5dd1674342523b65914d190e7f0ffeaebf3ef
Lavrov-Artyom <artem3sandy@gmail.com>
2020-12-09 19:26:06 +0300

    Fifth5

--- include/MyString.h
+++ include/MyString.h
@@ -10,29 +10,29 @@
 
 class MyString {
  private:
-	char* str;
+   char* str;
 
  public:
-	explicit MyString(const char* str = nullptr);
-	explicit MyString(std::string str);
-	MyString(MyString&&);
-	MyString(const MyString&);
-	char* get();
-	int length();
-	MyString operator+(MyString);
-	MyString operator-(MyString);
-	MyString operator*(int n);
-	int operator==(const MyString&);
-	int operator!=(const MyString&);
-	int operator>=(const MyString&);
-	int operator<=(const MyString&);
-	int operator>(const MyString&);
-	int operator<(const MyString&);
-	MyString& operator=(const MyString&);
-	MyString& operator=(MyString&&);
-	MyString& operator!();
-	char operator[](int n);
-	int operator()(const char*);
+   explicit MyString(const char* str = nullptr);
+   explicit MyString(std::string str);
+   MyString(MyString&&);
+   MyString(const MyString&);
+   char* get();
+   int length();
+   MyString operator+(MyString);
+   MyString operator-(MyString);
+   MyString operator*(int n);
+   int operator==(const MyString&);
+   int operator!=(const MyString&);
+   int operator>=(const MyString&);
+   int operator<=(const MyString&);
+   int operator>(const MyString&);
+   int operator<(const MyString&);
+   MyString& operator=(const MyString&);
+   MyString& operator=(MyString&&);
+   MyString& operator!();
+   char operator[](int n);
+   int operator()(const char*);
 };
 
 #endif  // INCLUDE_MYSTRING_H_
