62c0eb3fefffa0f2e5ccca476b7a2a0039199982
Ksuvot <ksuvot36@yandex.ru>
2020-12-09 12:12:59 +0300

    fix 1.1

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,12 @@
 // Copyright 2020 Ksuvot
 #include "MyString.h"
 
-int main()
-{
+int main() {
   MyString s1("Hello,");
   MyString s2("world!");
   MyString s3;
   s3 = s1 + s2;
   std::cout << s3;
-  std::cout << s3("ll");   // 2
-  std::cout << s3("mama"); // -1
+  std::cout << s3("ll");
+  std::cout << s3("mama");
 }
