8a9bb53909b9e170c64595eb36b61d0fa6f777a1
Vlad <zagoskin.3@mail.ru>
2020-12-13 20:25:40 +0300

    fixed cpplint

--- include/MyString.h
+++ include/MyString.h
@@ -12,7 +12,7 @@ class MyString {
  public:
         ~MyString();
         MyString();
-        explicit MyString(const char* str);
+        MyString(const char* str = nullptr);
         explicit MyString(std::string str);
         MyString(const MyString& str);
         MyString(MyString&& str);
