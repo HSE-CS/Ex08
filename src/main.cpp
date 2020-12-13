caa65f2855da07423f45862b5187fbb7d2c8b474
dsbarinov1 <dsbarinov_1@edu.hse.ru>
2020-12-13 23:55:07 +0300

    Style corrected, MyString fixed, main.cpp and MyString.h added

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+//  Copyright 2020 DBarinov
+#include "MyString.h"
+
+int main() {
+  MyString s1("Commit");
+  MyString s2("passed");
+  MyString s3;
+  s3 = s1 + s2;
+  std::cout << s3 << "\n";
+  std::cout << s3("ll") << "\n";  
+  std::cout << s3("fork") << "\n";  
+  return 0;
+}
