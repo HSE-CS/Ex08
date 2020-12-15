10c6e3c6d5cf929de37808713f159455ee2842b3
unknown <dakozlova_1@edu.hse.ru>
2020-12-16 01:29:48 +0300

    add main

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,22 @@
 #include "MyString.h"
 
 int main() {
+  MyString s1("1234");
+  MyString s2("12345");
+  MyString s3("12345");
+  std::cout << (s1 < s2) << std::endl;
+  std::cout << (s1 <= s2) << std::endl;
+  std::cout << (s1 > s2) << std::endl;
+  std::cout << (s1 >= s2) << std::endl;
+  std::cout << (s2 == s3) << std::endl;
+  std::cout << (s1 * 2) << std::endl;
+  std::cout << (s2 - s1) << std::endl;
+  std::cout << (s1 + s2) << std::endl;
+  MyString s4 = s1;
+  std::cout << s4 << std::endl;
+  MyString s5("aaaaa");
+  std::cout << s5 << std::endl;
+  s5[4] = 'm';
+  std::cout << s5 << std::endl;
   return 0;
 }
