a97fd330e52c686f2df75d96a7567bb007b6c0c9
Andrej <andrej_kostin@mail.ru>
2020-12-15 20:30:13 +0300

    fix bugs version 2

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,22 @@
 #include"MyString.h"
 
 int main() {
-    MyString string("Hello World!");
-    MyString string1("WoRLDDd");
-    MyString string2("World");
-    MyString string3;
-    MyString kk;
-    bool kiki = string != string2;
-    kk = !string;
-    std::cin >> string3;
-    std::cout << string3 << std::endl;
-    std::cout << "leng " << string3.length() << std::endl;
-    std::cout << string2("oki");
-    std::cout << std::endl;
-    std::cout << kk.get() << std::endl;
-    std::cout <<  kiki;
-
+//    MyString string("Hello World!");
+//    MyString string1("WoRLDDd");
+//    MyString string2("World");
+//    MyString string3;
+//    MyString kk;
+//    bool kiki = string != string2;
+//    kk = !string;
+//    std::cin >> string3;
+//    std::cout << string3 << std::endl;
+//    std::cout << "leng " << string3.length() << std::endl;
+//    std::cout << string2("oki");
+//    std::cout << std::endl;
+//    std::cout << kk.get() << std::endl;
+//    std::cout <<  kiki;
+    MyString a("abc");
+    MyString b;
+    b = !a;
+    std::cout << b.get();
 }
