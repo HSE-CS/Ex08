ac46e9192bf2524cefcd5518ffe191960ee7daf0
Alexander Grachev <s89519011368@yandex.ru>
2020-12-16 17:53:50 +0300

    first add

---
+++ include/MyString.h
@@ -0,0 +1,6 @@
+// Copyright 2020 Grachev Alexander
+
+#ifndef EX08_MYSTRING_H
+#define EX08_MYSTRING_H
+
+#endif //EX08_MYSTRING_H
---
+++ src/main.cpp
@@ -0,0 +1,3 @@
+// Copyright 2020 Grachev Alexander
+
+#include "../include/MyString.h"
