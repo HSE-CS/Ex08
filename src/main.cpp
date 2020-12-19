1876e39cd098c4d14706bda49e2cf565f2b04f7e
Right-bugor <71226571+Right-bugor@users.noreply.github.com>
2020-12-19 16:59:29 +0300

    try

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@ int main() {
     MyString s2("world!");
     MyString s3;
     s3 = !s1 + !s2;
-    std::cout << s1+ s2<<'\n';
     std::cout << s3 << '\n';
     s3 = s3 * 5;
     std::cout << s3 << '\n';
