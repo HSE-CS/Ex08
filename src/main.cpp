18db65e272cc68bd32a7938c4e78872fb5a5bcd0
Right-bugor <71226571+Right-bugor@users.noreply.github.com>
2020-12-19 16:45:38 +0300

    try

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,13 @@ int main() {
 	MyString s1("Hello,");
 	MyString s2("world!");
 	MyString s3;
-	
-	std::cout << s1<< s2<<'\n';
-	std::cout << s1("llo,");   // 2
-	std::cout << s3("mama"); // -1
+	s3=!s1+!s2
+	std::cout << s1+ s2<<'\n';
+	std::cout << s3 << '\n';
+	s3 = s3 * 5;
+	std::cout << s3 << '\n';
+	std::cout << s1("llo,")<<'\n';   // 2
+	std::cout << s3("mama")<<'\n'; // -1
 
 	return 0;
 }
