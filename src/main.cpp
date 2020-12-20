9ce0c7be8ab51fa2f2b25baa0bea208a57b45c4b
GDM1705 <71228914+GDM1705@users.noreply.github.com>
2020-12-20 20:47:30 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,18 @@
 #include <iostream>
 
 int main() {
-	MyString s1("Denis");
-	std::cout << s1.get() << std::endl;
-	MyString s2("Golovanov");
-	MyString s3 = s1 + s2;
-	std::cout << s3 << std::endl;
-	s3 = s1 * 2;
-	std::cout << s3 << std::endl;
-	s3 = s1 - s2;
-	std::cout << s3 << std::endl;
-	std::cout << (s1 > s2) << std::endl;
-	std::cout << (s1 == s2) << std::endl;
-	std::cout << (s1(s2.get())) << std::endl;
+  MyString s1("Denis");
+  std::cout << s1.get() << std::endl;
+  MyString s2("Golovanov");
+  MyString s3 = s1 + s2;
+  std::cout << s3 << std::endl;
+  s3 = s1 * 2;
+  std::cout << s3 << std::endl;
+  s3 = s1 - s2;
+  std::cout << s3 << std::endl;
+  std::cout << (s1 > s2) << std::endl;
+  std::cout << (s1 == s2) << std::endl;
+  std::cout << (s1(s2.get())) << std::endl;
 
-	return 0;
+  return 0;
 }
