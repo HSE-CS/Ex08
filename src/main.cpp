21b7cd4fd8a9052b59ffcb0691f98cc4bf9bad15
Karcevamd <mashkarcev@gmail.com>
2020-12-20 14:44:46 +0300

    19-AMI Kartseva Masha

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+// Copyright 2020 Kartseva Masha
+
+#include "MyString.h"
+
+int main() {
+	MyString s1("1234");
+	MyString s2("12345");
+	MyString s3("12345");
+	MyString s4 = s1 + s2;
+	MyString s5 = s2 - s1;
+	MyString s6 = s1 * 2;
+	std::cout << s4 << std::endl;
+	std::cout << s5 << std::endl;
+	std::cout << s6 << std::endl;
+	std::cout << (s1 < s2) << std::endl;
+	std::cout << (s1 <= s2) << std::endl;
+	std::cout << (s1 > s2) << std::endl;
+	std::cout << (s1 >= s2) << std::endl;
+	std::cout << (s2 == s3) << std::endl;
+	MyString s7 = s1;
+	std::cout << s7 << std::endl;
+	MyString s8("aaaaa");
+	std::cout << s8 << std::endl;
+	s8[4] = 'm';
+	std::cout << s8 << std::endl;
+	return 0;
+}
